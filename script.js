4c0574e7ae1bdbaf50101a9896b43a1a21ec38c3
Olivier_Luethy <olivier@kauz.ch>
2023-02-24 16:53:40 +0100

    Date added and styling changes

--- script.js
+++ script.js
@@ -17,24 +17,25 @@ submitBtn.addEventListener('click', async function() {
   
   // Check if the API returned valid data
   if (data.sys) {
-    document.getElementById("dataNotAvailable").innerHTML ="";
+    // Remove "No data available" error message if it was previously displayed
+    document.getElementById("dataNotAvailable").innerHTML = "";
+    
     // Convert the sunrise and sunset timestamps to local time and format as a string
     const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
     const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
-    
-    document.getElementById("sunrise-image").src="images/sunrise icon.png";
-    document.getElementById("sunset-image").src="images/sunset icon.png";
 
-    // Update the HTML output with the sunrise and sunset times
-    sunriseOutput.textContent = `Sunrise: ${sunrise}`;
-    sunsetOutput.textContent = `Sunset: ${sunset}`;    
+    // Get the current date and format as a string
+    const currentDate = new Date().toLocaleDateString();
+
+    // Update the HTML output with the sunrise and sunset times and current date
+    sunriseOutput.innerHTML = `<img src="images/sunrise icon.png" alt="Sunrise icon"><br>Sunrise: ${sunrise}<br>Date: ${currentDate}`;
+    sunsetOutput.innerHTML = `<img src="images/sunset icon.png" alt="Sunset icon"><br> Sunset: ${sunset}<br>Date: ${currentDate}`;
   } else {
     // If the API did not return valid data, update the HTML output with an error message
-    sunriseOutput.textContent = "";
-    sunsetOutput.textContent = "";
-    document.getElementById("sunrise-image").src="";
-    document.getElementById("sunset-image").src="";
-
-    document.getElementById("dataNotAvailable").innerHTML ="No data available";
+    sunriseOutput.innerHTML = "";
+    sunsetOutput.innerHTML = "";
+    document.getElementById("sunrise-image").src = "";
+    document.getElementById("sunset-image").src = "";
+    document.getElementById("dataNotAvailable").innerHTML = "No data available";
   }
 });
